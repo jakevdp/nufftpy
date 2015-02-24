11abb3dc83346d88eb7c7099623bcff067cfe733
Jake VanderPlas <jakevdp@gmail.com>
2015-02-23 17:19:26 -0800

    use skiptest if numba not installed

--- nufftpy/tests/test_nufft1d.py
+++ nufftpy/tests/test_nufft1d.py
@@ -6,12 +6,20 @@ from numpy.testing import assert_allclose
 from nose import SkipTest
 
 
+try:
+    import numba
+except ImportError:
+    numba = None
+
+
 def test_nufft1_vs_direct_1D():
     rng = np.random.RandomState(0)
     x = 100 * rng.rand(100)
     c = np.exp(1j * x)
 
     def check_results(df, iflag, M, eps, use_numba, fast_gridding):
+        if use_numba and numba is None:
+            raise SkipTest("numba is not installed")
         dft = nufft1(x, c, M, iflag=iflag, direct=True)
         fft = nufft1(x, c, M, iflag=iflag, eps=eps,
                      use_numba=use_numba, fast_gridding=fast_gridding)
@@ -38,8 +46,10 @@ def test_nufft1_vs_fortran_1D():
     x = 100 * rng.rand(100)
     c = np.exp(1j * x)
 
-    def check_results(df, iflag, M, eps):
-        fft1 = nufft1(x, c, M, iflag=iflag, eps=eps)
+    def check_results(df, iflag, M, eps, use_numba):
+        if use_numba and numba is None:
+            raise SkipTest("numba is not installed")
+        fft1 = nufft1(x, c, M, iflag=iflag, eps=eps, use_numba=use_numba)
         fft2 = nufft1_fortran(x, c, M, iflag=iflag, eps=eps)
         assert_allclose(fft1, fft2)
 
@@ -47,7 +57,8 @@ def test_nufft1_vs_fortran_1D():
         for iflag in [1, -1]:
             for M in [51, 100, 111]:
                 for eps in [1E-8, 1E-12]:
-                    yield check_results, df, iflag, M, eps
+                    for use_numba in [True, False]:
+                        yield check_results, df, iflag, M, eps, use_numba
 
 
 def test_nufft1_fastgridding_1D():
@@ -56,6 +67,8 @@ def test_nufft1_fastgridding_1D():
     c = np.exp(1j * x)
 
     def check_results(df, iflag, M, eps, use_numba):
+        if use_numba and numba is None:
+            raise SkipTest("numba is not installed")
         fft1 = nufft1(x, c, M, iflag=iflag, eps=eps,
                       use_numba=use_numba, fast_gridding=True)
         fft2 = nufft1(x, c, M, iflag=iflag, eps=eps,
